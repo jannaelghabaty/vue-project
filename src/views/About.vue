0f963bc9dd69e732b8ea9be8a56552ceb66730c1
jannaelghabaty <janna.elghabaty@robco.de>
2024-10-03 23:02:17 +0200

    Feat(Footer): Add Footer design and functionality

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup></script>
 
 <template>
-    <h1>About View Page</h1>
+  <h1>About View Page</h1>
 </template>
--- src/views/GamesLibrary.vue
+++ src/views/GamesLibrary.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup></script>
 
 <template>
-    <h1>Games Library View Page</h1>
+  <h1>Games Library View Page</h1>
 </template>
